5bcebbf0c84ace5854bcd1b2933e03e9b0ad6df4
Francisco Leyva <leyvajustin@rocketmail.com>
2023-10-25 17:38:13 -0700

    fixed helprequest and repository
    added imports

--- src/main/java/edu/ucsb/cs156/example/repositories/HelpRequestRepository.java
+++ src/main/java/edu/ucsb/cs156/example/repositories/HelpRequestRepository.java
@@ -1,6 +1,11 @@
+package edu.ucsb.cs156.example.repositories;
 
+import edu.ucsb.cs156.example.entities.HelpRequest;
 
-@repository
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface HelpRequestRepository extends CrudRepository<HelpRequest, Long> {
 
     Iterable<HelpRequest> findAllByRequesterEmail(String requesterEmail);
