6897b8cd66d1ee1c1dec9d4c89c95d01dc082d7e
kendrick-lee <54284148+kendrick-lee@users.noreply.github.com>
2023-10-31 17:32:07 -0700

    Update UCSBOrganizationControllerTests.java

--- src/test/java/edu/ucsb/cs156/example/controllers/UCSBOrganizationControllerTests.java
+++ src/test/java/edu/ucsb/cs156/example/controllers/UCSBOrganizationControllerTests.java
@@ -123,7 +123,7 @@ public class UCSBOrganizationControllerTests extends ControllerTestCase {
 
                 // act
                 MvcResult response = mockMvc.perform(
-                        post("/api/ucsborganization/post?orgCode=OSLI&orgTranslationShort=STUDENT LIFE&orgTranslation=OFFICE OF STUDENT LIFE&inactive=false")
+                        post("/api/ucsborganization/post?orgCode=OSLI&orgTranslationShort=STUDENT LIFE&orgTranslation=OFFICE OF STUDENT LIFE&inactive=true")
                         .with(csrf())).andExpect(status().isOk()).andReturn();
 
                 // assert
